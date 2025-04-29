74e1c1a9fc5eadb158ec087ef96c07c7a14b2ef4
Michael Webb <michaelwebb76@gmail.com>
2025-04-29 11:15:48 +1000

    Try to nixify the repository and enjoy the heat emanating off my laptop as it builds Node from source

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-24.11-darwin";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        ciDeps = with pkgs; [
+          nodejs
+          nodePackages.prettier
+        ];
+      in
+      {
+        devShells = {
+          # Used in CI
+          default = pkgs.mkShell {
+            name = "schemas-env";
+            packages = ciDeps;
+          };
+
+          # recommended .envrc: use flake .#development
+          development = pkgs.mkShell {
+            packages =
+              ciDeps
+              ++ (with pkgs; [
+
+              ]);
+          };
+        };
+      }
+    );
+}
